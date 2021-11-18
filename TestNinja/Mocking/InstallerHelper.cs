7c081488aaa77b46f3e7afd7aace0b85ffc0d911
AndriiS <andrej.serga@gmail.com>
2021-11-19 01:15:20 +0200

    Add InstallerHelper; Add InstallerHelperTests;

---
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public class InstallerHelper
+    {
+        private readonly IFileDownloader _fileDownloader;
+        private readonly string _setupDestinationFile;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            _fileDownloader = fileDownloader;
+        }
+
+        public bool DownloadInstaller(string customerName, string installerName)
+        {
+            var url = String.Format("http://example.com/{0}/{1}",
+                    customerName,
+                    installerName);
+            try
+            {
+                _fileDownloader.DownloadFile(url, _setupDestinationFile);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+    }
+}
